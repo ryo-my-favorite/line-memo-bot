643f35553efcd9a9b0c1d5748a9cc38983451e7d
ryo-my-favorite <tekunsinfo@yahoo.co.jp>
2019-10-27 13:03:02 +0900

    build local lambda func

--- handler.ts
+++ handler.ts
@@ -1,12 +1,49 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyHandler, APIGatewayEvent, Context, APIGatewayProxyCallback } from 'aws-lambda';
 import 'source-map-support/register';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
+const https = require('https');
+
+let send = (data, callback) => {
+  let body = JSON.stringify(data);
+
+  let req = https.request({
+    hostname: 'api.line.me',
+    port: 443,
+    path: '/v2/bot/message/reply',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(body),
+      'Authorization': 'Bearer ' + process.env.CHANNEL_ACCESS_TOKEN
+    }
+  });
+
+  req.end(body, (err) => {
+    err && console.log(err);
+    callback(err);
+  });
+}
+
+export const main: APIGatewayProxyHandler = (event: APIGatewayEvent, context: Context, callback: APIGatewayProxyCallback) => {
+  //console.log(event.body);
+  let body = JSON.parse(event.body);
+  console.log('text is ' + body.events[0].message.text);
+  let result = body.events && body.events[0];
+  if (result) {
+    let content = body.events[0] || {};
+    let message = {
+      "replyToken": result.replyToken,
+      "message": [
+        {
+          "type": "text",
+          "text": content.message.text //chatメッセージをそのまま返す
+        }
+      ]
+    };
+    send(message, () => {
+      callback();
+    })
+  } else {
+    callback();
+  }
 }
